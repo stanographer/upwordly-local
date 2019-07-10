6d1ae07d4e9de3ba63b6bbd22d1773aa40a3dabe
Stanley Sakai <stanleysakai@Stanleys-MacBook-Pro.local>
2019-07-10 18:36:23 -0400

    Docker image

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,6 @@ import Document, {
 } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {...initialProps};
-  }
-
   render() {
     return (
         <Html>
