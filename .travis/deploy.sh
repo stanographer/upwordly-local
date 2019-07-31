b0162028bea19995910ff83dcfe50c3d164cdae5
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 00:12:29 -0400

    Changed the build directory from build to .next

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
 
-tar -czf package.tgz build && \
+tar -czf package.tgz .next && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
