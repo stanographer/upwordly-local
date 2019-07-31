9834b42c63be05d76274ceef987708a4b52c0cb0
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 15:15:10 -0400

    Deleted strict host key checking

--- .travis/untar.sh
+++ .travis/untar.sh
@@ -5,7 +5,7 @@ export NODE_ENV=production
 #export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
 cd /var/www/upword.ly && \
-tar zxvf package.tgz -C . && \
+tar -zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
 npm run start
