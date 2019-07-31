140d92c112224658d1d597ac43e22083898a3fe7
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 13:45:29 -0400

    Fixed Travis directory location.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,5 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   tar -czf package.tgz build && \
   sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
   sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
+  exit 0
 fi
