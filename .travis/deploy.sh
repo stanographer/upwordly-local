b24271466895eda1fc635366a70897a1075d0b73
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 13:59:52 -0400

    Fixed deploy script that was referring to ./travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   set -x
-
   tar -czf package.tgz build && \
   sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-  sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
+  sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < .travis/untar.sh
   exit 0
 fi
