2525bb38ba069b88fe7ca9f33e59e8934a31092a
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 13:29:13 -0400

    deploy command

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
 
-tar -czf package.tgz ./ && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
+tar -czf package.tgz build && \
+sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
