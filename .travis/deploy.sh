483e746e6edf4c214272c6cca353bba901efae9e
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 13:39:29 -0400

    Added deploy script to after_success

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
-set -x
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  set -x
 
-tar -czf package.tgz build && \
-sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
+  tar -czf package.tgz build && \
+  sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+  sshpass -p $PASSKEY scp -o StrictHostKeyChecking=no ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./travis/untar.sh
+fi
