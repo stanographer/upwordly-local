a95d938aba5d23828d10cc6188f2ecb5929469d3
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-07-31 21:51:22 -0400

    Added sudo -t

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,4 @@ mkdir latest
 mv !(latest) latest
 tar -czf package.tgz latest
 scp -o stricthostkeychecking=no package.tgz "$REMOTE_USER@$REMOTE_HOST:~/builds" || exit 1
-ssh -t -o stricthostkeychecking=no "$REMOTE_USER@$REMOTE_HOST" 'bash -s' < .travis/untar.sh
+ssh -t -o stricthostkeychecking=no "$REMOTE_USER@$REMOTE_HOST" 'bash -s -t' < .travis/untar.sh
