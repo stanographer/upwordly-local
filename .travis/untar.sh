8ea8089791f1232371a93a4951b465dcaa092eb2
Stanley Sakai <stanleysakai@Stanleys-MBP.fios-router.home>
2019-08-01 16:43:03 -0400

    Removed sudo commands from untar.sh

--- .travis/untar.sh
+++ .travis/untar.sh
@@ -5,9 +5,6 @@ echo "Now extracting and deploying..."
 # Debug mode so we can see what's happening.
 set -x
 
-# Install latest pm2
-sudo npm install pm2@latest -g
-
 # Move into builds folder
 cd ~/builds || exit 1
 
@@ -24,7 +21,7 @@ cd latest || exit 1
 npm install
 
 # Stop the running old deployment.
-sudo pm2 stop 'upwordly-frontend:latest'
+pm2 stop 'upwordly-frontend:latest'
 
 # Start a new process in cluster mode using all available processors.
-sudo pm2 start -i "max" npm --name "upwordly-frontend:latest" -- start
+pm2 start -i "max" npm --name "upwordly-frontend:latest" -- start
