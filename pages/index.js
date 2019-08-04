c44d5c804ae873aad1c04bb8c35c6cb8436f713f
Nel Malikova <gunelmalikova@gmail.com>
2019-08-03 22:45:13 -0400

    landing page tweaks

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,46 @@ import Typed from 'react-typed';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Provider from '../src/components/Session/Provider';
+import Logo from '../src/components/Logos/Logo'
+import Footer from '../src/components/General/Footer'
 
 const Index = () => {
   const router = useRouter();
   return (
     <Provider>
       <div className="landing-wrapper" id="landing-wrapper">
-        <section className="intro">
-          <div
-            className="content
-              inline-block
-              align-middle"
-          >
-            <h1 className="text-5xl landing-title">UPWORDLY</h1>
-            <p>
-              <Typed
-                className="marquee"
-                strings={[
-                  'One platform to sync, send, and integrate your real-time transcriptions.',
-                  'Web-based live stenography, anywhere.',
-                ]}
-                typeSpeed={40}
+        <section className="intro my-auto">
+          <Logo center={true} />
+          <div className="mt-5">
+            <Typed
+                className="marquee text-teal-200 sm:text-lg lg:text-2xl"
+                data-test="typed-text"
+                strings={['One platform to sync, send, and integrate your real-time transcriptions.', 'Web-based live stenography, anywhere.']}
+                typeSpeed={60}
+                loop
               />
-            </p>
-            <p
-              style={{
-                padding: '0 4rem',
-              }}
-            >
-              <Link href="/view?user=stanley&job=mocj">
-                <a>hello</a>
-              </Link>
-              Upwordly is a real-time transcription delivery tool and a content
-              management system (CMS) for real-time stenographers. You login,
+          </div>
+          <p className="w-9/12 text-center mx-auto my-5 sm:text-lg lg:text-2xl">
+              Upword.ly is a real-time transcription delivery tool and a content
+              management system (CMS) for real-time stenographers. You log in,
               create a job, connect your CAT software via a small helper app,
               and start writing. Distribute your live feed to anyone via a short
-              URL and that's it! Since it is web based, there's no need for your
+              URL and that's it! Since it is web-based, there's no need for your
               consumers to install anything on their end. When the job is over,
               it's your choice to use the variety of tools to save or
               disseminate the transcript or to delete it. Simple.
-              <Link href="/dashboard" prefetch>
-                <a>Dashboard</a>
-              </Link>
-            </p>
-          </div>
+          </p>
+          <Footer/>
+            {/*<Link href="/dashboard" prefetch>*/}
+            {/*  <a>Dashboard</a>*/}
+            {/*</Link>*/}
         </section>
       </div>
     </Provider>
   );
 };
 
-Index.getInitialProps = async ({ query, res }) => {
+Index.getInitialProps = async ({query, res}) => {
   console.log('query, res', query, res);
 };
 
