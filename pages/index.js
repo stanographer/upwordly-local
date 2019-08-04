6f5ecb2709c9ee76bbc8350330485fcae5c3c1b4
Nel Malikova <gunelmalikova@gmail.com>
2019-08-04 00:03:05 -0400

    Add signup and signin links to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import Typed from 'react-typed';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import Provider from '../src/components/Session/Provider';
 import Logo from '../src/components/Logos/Logo'
 import Footer from '../src/components/General/Footer'
 
 const Index = () => {
-  const router = useRouter();
   return (
     <Provider>
-      <div className="landing-wrapper" id="landing-wrapper">
-        <section className="intro my-auto">
+      <div className='landing-wrapper' id='landing-wrapper'>
+        <section className='intro my-auto'>
           <Logo center={true} />
-          <div className="mt-5">
+          <div className='mt-5'>
             <Typed
-                className="marquee text-teal-200 sm:text-lg lg:text-2xl"
-                data-test="typed-text"
+                className='marquee text-teal-200 sm:text-lg lg:text-2xl'
+                data-test='typed-text'
                 strings={['One platform to sync, send, and integrate your real-time transcriptions.', 'Web-based live stenography, anywhere.']}
                 typeSpeed={60}
                 loop
               />
           </div>
-          <p className="w-9/12 lg:w-7/12 text-center mx-auto my-5 sm:text-lg lg:text-2xl">
+          <p className='w-9/12 lg:w-7/12 text-center mx-auto my-5 sm:text-lg lg:text-2xl'>
               Upword.ly is a real-time transcription delivery tool and a content
               management system (CMS) for real-time stenographers. You log in,
               create a job, connect your CAT software via a small helper app,
@@ -32,10 +30,23 @@ const Index = () => {
               it's your choice to use the variety of tools to save or
               disseminate the transcript or to delete it. Simple.
           </p>
+            <Link href='/signin' prefetch={true}>
+              <button
+                title='sign in'
+                className='bg-blue hover:bg-blue-dark text-teal-200 font-bold md:text-lg py-2 px-4 mx-2 rounded border-white hover:border-transparent hover:text-bg hover:bg-teal-200'
+                type='button'>
+                Sign In
+              </button>
+            </Link>
+            <Link href='/signup' prefetch={true}>
+              <button
+                title='sign up'
+                className='bg-blue hover:bg-blue-dark text-teal-200 font-bold md:text-lg py-2 px-4 mx-2 rounded border-white hover:border-transparent hover:text-bg hover:bg-teal-200'
+                type='button'>
+                Sign Up
+              </button>
+            </Link>
           <Footer />
-          {/*<Link href="/dashboard" prefetch>*/}
-          {/*  <a>Dashboard</a>*/}
-          {/*</Link>*/}
         </section>
       </div>
     </Provider>
