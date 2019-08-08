879157b9a68c54938826493692f229aa3acd60db
Stanley Sakai <stanleysakai@Stanleys-MacBook-Pro.local>
2019-08-08 11:12:44 -0400

    new favicons.

--- pages/_document.js
+++ pages/_document.js
@@ -24,84 +24,92 @@ class MyDocument extends Document {
             content="Upword.ly - Cloud-Based Transcription CMS for Live Captioners and Stenographers"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="57x57"
-            href="/static/img/favicons//static/img/favicons/apple-icon-57x57.png"
+            href="/static/favicons/apple-icon-57x57.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="60x60"
-            href="/static/img/favicons//static/img/favicons/apple-icon-60x60.png"
+            href="/static/favicons/apple-icon-60x60.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="72x72"
-            href="/static/img/favicons/apple-icon-72x72.png"
+            href="/static/favicons/apple-icon-72x72.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="76x76"
-            href="/static/img/favicons/apple-icon-76x76.png"
+            href="/static/favicons/apple-icon-76x76.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="114x114"
-            href="/static/img/favicons/apple-icon-114x114.png"
+            href="/static/favicons/apple-icon-114x114.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="120x120"
-            href="/static/img/favicons/apple-icon-120x120.png"
+            href="/static/favicons/apple-icon-120x120.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="144x144"
-            href="/static/img/favicons/apple-icon-144x144.png"
+            href="/static/favicons/apple-icon-144x144.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="152x152"
-            href="/static/img/favicons/apple-icon-152x152.png"
+            href="/static/favicons/apple-icon-152x152.png"
           />
           <link
-            rel="/static/img/favicons/apple-touch-icon"
+            rel="/static/favicons/apple-touch-icon"
             sizes="180x180"
-            href="/static/img/favicons/apple-icon-180x180.png"
+            href="/static/favicons/apple-icon-180x180.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="192x192"
-            href="/static/img/favicons/android-icon-192x192.png"
+            href="/static/favicons/android-icon-192x192.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/img/favicons/favicon-32x32.png"
+            href="/static/favicons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="96x96"
-            href="/static/img/favicons/favicon-96x96.png"
+            href="/static/favicons/favicon-96x96.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/img/favicons/favicon-16x16.png"
+            href="/static/favicons/favicon-16x16.png"
           />
           <link
             rel="shortcut icon"
             type="image/x-icon"
-            href="/static/img/favicons/favicon.ico"
+            href="/static/favicons/favicon.ico"
           />
-          <link rel="manifest" href="/static/img/favicons/manifest.json" />
+          <link rel="manifest" href="/static/favicons/manifest.json" />
           <meta name="msapplication-TileColor" content="#282c34" />
           <meta
             name="msapplication-TileImage"
-            content="/static/img/favicons/ms-icon-144x144.png"
+            content="/static/favicons/ms-icon-144x144.png"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="/static/favicons/ms-icon-150x150.png"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="/static/favicons/ms-icon-310x310.png"
           />
           <meta name="theme-color" content="#282c34" />
           <style>{'body { margin: 0 } /* custom! */'}</style>
