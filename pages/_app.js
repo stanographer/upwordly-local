0ad223e615d32445bbaf722fa7dc1f62a2361b8d
Stanley Sakai <stanley@stanographer.com>
2020-01-09 21:42:22 -0500

    Removed the user view.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import App from 'next/app';
-import dynamic from 'next/dynamic';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-dynamic(() => import('../public/static/styles/style.css'));
-dynamic(() => import('../public/static/styles/fonts.css'));
+import '../public/static/styles/style.css';
+import '../public/static/styles/fonts.css';
+// dynamic(() => import('../public/static/styles/style.css'));
+// dynamic(() => import('../public/static/styles/fonts.css'));
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
