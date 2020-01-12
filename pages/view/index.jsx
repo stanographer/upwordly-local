5249136c4f74a2c308150ef0cd53eb5a50615364
Stanley Sakai <stanley@stanographer.com>
2020-01-12 16:38:45 -0500

    Removed all routing.

--- pages/view/index.jsx
+++ pages/view/index.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { withRouter } from 'next/router';
 import ViewComponent from '../../src/components/View';
 
-function View(props) {
-  const { router } = props;
-  return (
-    <div>
-      <p>
-        {router.query.user} {router.query.job}
-      </p>
-    </div>
-  );
-}
+// function View(props) {
+//   const { router } = props;
+//   return (
+//     <div>
+//       <p>
+//         {router.query.user} {router.query.job}
+//       </p>
+//     </div>
+//   );
+// }
+//
+// export default withRouter(View);
 
-export default withRouter(View);
+const View = () => {
+  return <div>This is the view page yo!</div>;
+};
+
+export default View;
