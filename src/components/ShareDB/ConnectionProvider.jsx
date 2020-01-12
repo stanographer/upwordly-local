d106ab6b4fcf9dfc91d7d8c2d948fc9fa322a997
Stanley Sakai <stanley@stanographer.com>
2020-01-12 18:41:58 -0500

    Moved binding out of useEffect

--- src/components/ShareDB/ConnectionProvider.jsx
+++ src/components/ShareDB/ConnectionProvider.jsx
@@ -16,66 +16,64 @@ class ConnectionProvider extends Component {
     super(props, context);
 
     this.state = {
-      ...INITIAL_STATE
+      ...INITIAL_STATE,
     };
 
     this.socket = {};
     this.connection = {};
   }
 
-  fetchDoc = host => {
-    const {connected} = this.state;
-
-    this.assembleUrl(host)
-        .then(url => {
-
-          // Create a reconnecting websocket connection.
-          this.socket = new ReconnectingWebSocket(url, [], {
-            WebSocket: WS,
-            connectionTimeout: 1000,
-            minReconnectionDelay: 400 + Math.random() * 4000,
-            minUptime: 8000,
-            maxReconnectionDelay: 3000,
-            maxRetries: Infinity,
-            reconnectionDelayGrowFactor: 1.2,
-          });
-
-          // To display a message to the user that the connection is re-established.
-          // this.socket.onopen = () => {
-          //   console.log('ok ok!');
-          //   if (!connected) {
-          //     this.props.onReconnect();
-          //     this.setState({
-          //       connected: true,
-          //     });
-          //   }
-          // };
-
-          // Register text OT type.
-          ShareDB.types.register(otText.type);
-
-          // Establish connection with ShareDB server.
-          this.connection = new ShareDB.Connection(this.socket);
-
-          this.setState({
-            connection: this.connection,
-            connected: true,
-            doc: this.connection.get(this.props.user, this.props.job),
-          });
-        });
-  };
-
   // Outputs the websockets URI depending on app environment.
   assembleUrl = async host => {
-    const domain = process.env.NODE_ENV === 'production' ? 'upword.ly/ws' : host;
+    const domain =
+      process.env.NODE_ENV === 'production' ? 'upword.ly/ws' : host;
     const port = process.env.NODE_ENV === 'production' ? '' : ':9090';
     const protocol = process.env.NODE_ENV === 'production' ? 'wss://' : 'ws://';
 
     return protocol + domain + port;
   };
 
-  componentDidMount() {
+  fetchDoc = host => {
+    const { connected } = this.state;
 
+    this.assembleUrl(host).then(url => {
+      // Create a reconnecting websocket connection.
+      this.socket = new ReconnectingWebSocket(url, [], {
+        WebSocket: WS,
+        connectionTimeout: 1000,
+        minReconnectionDelay: 400 + Math.random() * 4000,
+        minUptime: 8000,
+        maxReconnectionDelay: 3000,
+        maxRetries: Infinity,
+        reconnectionDelayGrowFactor: 1.2,
+      });
+
+      // To display a message to the user that the connection is re-established.
+      // this.socket.onopen = () => {
+      //   console.log('ok ok!');
+      //   if (!connected) {
+      //     this.props.onReconnect();
+      //     this.setState({
+      //       connected: true,
+      //     });
+      //   }
+      // };
+
+      // Register text OT type.
+      ShareDB.types.register(otText.type);
+
+      // Establish connection with ShareDB server.
+      this.connection = new ShareDB.Connection(this.socket);
+
+      this.setState({
+        connection: this.connection,
+        connected: true,
+        doc: this.connection.get(this.props.user, this.props.job),
+      });
+    });
+  };
+
+  componentDidMount() {
     // Subscribe to document.
     this.fetchDoc(window.location.hostname);
   }
@@ -86,17 +84,10 @@ class ConnectionProvider extends Component {
   }
 
   render() {
-    const {connected} = this.state;
-    console.log(this.props.user, this.props.job, this.state.doc);
+    const { connected } = this.state;
 
     return (
-        <Fragment>
-          {
-            connected
-                ? this.props.render(this.state)
-                : ''
-          }
-        </Fragment>
+      <Fragment>{connected ? this.props.render(this.state) : ''}</Fragment>
     );
   }
 }
--- src/components/ShareDB/Document.jsx
+++ src/components/ShareDB/Document.jsx
@@ -6,27 +6,27 @@ import { FetchingToast, LoadedToast } from '../Toasts';
 function Document(props) {
   const { doc, flag } = props;
   const [text, setText] = useState('');
+  let binding;
+
+  FetchingToast();
+
+  doc.subscribe(err => {
+    if (err) {
+      setText(`There was a connection error: ${err}`);
+    }
+  });
+
+  // Load document and bind it to local snapshot.
+  doc.on('load', () => {
+    binding = new Binding(doc.data, flag);
+
+    setTimeout(() => {
+      setText(binding.snapshot || 'Connection successful.');
+      LoadedToast();
+    }, 0);
+  });
 
   useEffect(() => {
-    let binding;
-    FetchingToast();
-
-    doc.subscribe(err => {
-      if (err) {
-        setText(`There was a connection error: ${err}`);
-      }
-    }, []);
-
-    // Load document and bind it to local snapshot.
-    doc.on('load', () => {
-      binding = new Binding(doc.data, flag);
-
-      setTimeout(() => {
-        setText(binding.snapshot || 'Connection successful.');
-        LoadedToast();
-      }, 0);
-    });
-
     // Apply remote ops to local snapshot.
     doc.on('op', op => {
       setTimeout(() => {
@@ -44,10 +44,5 @@ function Document(props) {
 
   return <section className="transcript">{text || ''}</section>;
 }
-
-Document.propTypes = {
-  doc: PropTypes.object,
-  flag: PropTypes.string,
-};
 
 export default Document;
