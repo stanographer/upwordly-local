f674d91b95e124c5a288cbd572b39e7bdb548b83
Stanley Sakai <stanley@stanographer.com>
2020-01-12 17:25:30 -0500

    Added improper formatted watch page warning.

--- pages/watch/[user]/[job].jsx
+++ pages/watch/[user]/[job].jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Watch = props => {
   const { user, job } = props;
   return (
-    <div>
-      User: {user}, job: {job}
-    </div>
+    <Fragment>
+      {user && job ? (
+        <div>
+          User: {user}, job: {job}
+        </div>
+      ) : (
+        <div>
+          <h2>URL incorrectly formatted</h2>
+          <p>
+            Live transcription URLs must be in the form:{' '}
+            <i>upword.ly/watch/[username]/[job slug]</i>.
+          </p>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
--- pages/watch/index.jsx
+++ pages/watch/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
+import Button from '../../src/components/Dashboard/Button';
 
 const Watch = () => {
-  return <div>This is the watch page yo!</div>;
+  return (
+    <div className="m-8">
+      <h2 className="text-4xl">URL incorrectly formatted</h2>
+      <p className="text-2xl">
+        Live transcription URLs must be in the form:{' '}
+        <i>upword.ly/watch/[username]/[job slug]</i>.
+      </p>
+      <Link href="/">
+        <Button
+          color="green"
+          classNames="mx-0 my-6"
+          text="Return to landing page"
+          type="button"
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default Watch;
